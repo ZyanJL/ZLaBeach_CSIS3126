df981e9eca3711d9ca6d9909f28827132167cc43
ZyanJL <50376469+ZyanJL@users.noreply.github.com>
2022-02-11 03:10:07 -0500

    Add files via upload

--- sample_process.php
+++ sample_process.php
@@ -7,7 +7,7 @@ echo "Hello, " . $_POST["firstname"];
 
 //connect to the database
 
-$connection = mysqli_connect("localhost", "root", "root", "Athletes");
+$connection = mysqli_connect("localhost", "root", "root", "athletes") or die(mysqli_error($connection));
 
 // Check connection
 if ($conn->connect_error) {
@@ -18,13 +18,13 @@ echo "\nConnected successfully";
 //add this person to the database
 
 $firstname = mysqli_real_escape_string($connection,$_POST["firstname"]);
-mysqli_query($connection,"insert into Athletes_Recorded (firstname) values ('$firstname')");
+mysqli_query($connection,"insert into athlete (firstname) values ('$firstname')") or die(mysqli_error($connection));
 
 
 //show a list of people in the database
 echo "<br /><br />Athletes already in the database: <br />";
 
-$res = mysqli_query($connection,"select * from Athletes_Recorded");
+$res = mysqli_query($connection,"select * from athlete");
 while ($row = mysqli_fetch_assoc($res)) {
     echo $row["id"] . " " . $row["firstname"] . "<br />";
 }
