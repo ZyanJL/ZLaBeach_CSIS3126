d5737f4e6583dd803f8d5901f8c0c20b9f228c6b
ZyanJL <50376469+ZyanJL@users.noreply.github.com>
2022-03-23 03:54:33 -0400

    Add files via upload

--- demo_store.php
+++ demo_store.php
@@ -1,13 +1,14 @@
 <?php
 
+include 'session.php';
 
 
 
 $data = $_POST["data"];
 
+$filename = 'user_name' + time() + '.txt';  
 
-
-file_put_contents("data.txt",$data);
+file_put_contents($filename,$data);
 
 
 ?>
--- welcome.php
+++ welcome.php
@@ -1,9 +1,9 @@
 <?php 
 
-session_start();
+include 'session.php';
 
 if (!isset($_SESSION['user_name'])) {
-    header("Location: index.php");
+    header("Location: login.php");
 }
 
 ?>
@@ -17,6 +17,7 @@ if (!isset($_SESSION['user_name'])) {
 </head>
 <body>
     <?php echo "<h1>Welcome " . $_SESSION['user_name'] . "</h1>"; ?>
+    <a href="phone_swing.php">PhoneSwing</a>
     <a href="logout.php">Logout</a>
 </body>
 </html>
