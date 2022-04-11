0aff0dde149c15b89035c2b3cedd1934f222e5a0
Zyan LaBeach <zyjordin1005@gmail.com>
2022-04-11 17:36:32 -0400

    testing git

--- welcome.php
+++ welcome.php
@@ -18,7 +18,7 @@ if (!isset($_SESSION['user_name'])) {
 </head>
 
 <body>
-    <?php echo "<h1>Welcome " . $_SESSION['user_name'] . "</h1>"; ?>
+    <?php echo "<h1>Welcome to the database " . $_SESSION['user_name'] . "</h1>"; ?>
     <a href="phone_swing.php">PhoneSwing</a>
     <a href="view_swings.php">view all swings</a>
     <a href="logout.php">Logout</a>
