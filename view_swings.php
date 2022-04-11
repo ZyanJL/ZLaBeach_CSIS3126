ef27ed93b02ba7d2f97d3ae2c936f931905bf9d5
Zyan LaBeach <zyjordin1005@gmail.com>
2022-04-11 18:20:22 -0400

    nothing new

--- view_swings.php
+++ view_swings.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php 
 include 'session.php';
 ?>
@@ -47,54 +46,4 @@ include 'session.php';
 
 </body>
 
-=======
-<?php 
-include 'session.php';
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome</title>
-</head>
-
-<body>
-    <?php echo "<h1>Welcome " . $_SESSION['user_name'] . "</h1>"; ?>
-    <a href="phone_swing.php">PhoneSwing</a>
-    <a href="logout.php">Logout</a>
-
-    <?php
-
-    $user_id = $_SESSION['user_id'];
-    $user_name = $_SESSION['user_name'];
-
-    //query for all swings
-    $sql = "SELECT filename FROM phone_swings WHERE user_id = " . $user_id;
-   
-    //make query and get result
-    $result = mysqli_query($conn, $sql); 
-
-    //fetch the resulting rows as an array of filenames
-    if ($result->num_rows > 0) {
-        //output data
-        while ($row = $result->fetch_assoc()) {
-            echo "<br>" . "<a href='print_charts.php?file=" .  $row["filename"] . "'>" .  $row["filename"] . "</a>";
-        }
-    } else {
-        echo "0 results";
-    }
-
-    //free results from memory
-    mysqli_free_result($result);
-
-    //print_r($swings);
-
-    ?>
-
-
-</body>
-
->>>>>>> f868acfa2c31981e5d363b88635a05be2610137c
 </html>
--- welcome.php
+++ welcome.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 include 'session.php';
@@ -25,28 +24,4 @@ if (!isset($_SESSION['user_name'])) {
     <a href="logout.php">Logout</a>
 </body>
 
-=======
-<?php 
-
-include 'session.php';
-
-if (!isset($_SESSION['user_name'])) {
-    header("Location: login.php");
-}
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome</title>
-</head>
-<body>
-    <?php echo "<h1>Welcome " . $_SESSION['user_name'] . "</h1>"; ?>
-    <a href="phone_swing.php">PhoneSwing</a>
-    <a href="logout.php">Logout</a>
-</body>
->>>>>>> f868acfa2c31981e5d363b88635a05be2610137c
 </html>
